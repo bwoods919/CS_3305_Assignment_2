badbfa69eaf2a007c057fb418de7ed9d86e269d7
bwood <bwoods51501@gmail.com>
2022-08-24 15:03:13 -0400

    Changes

--- src/Vowels.java
+++ src/Vowels.java
@@ -5,5 +5,50 @@
 // Assignment: 2
 // IDE Name: IntelliJ
 
+import java.util.Scanner;
+
 public class Vowels {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        boolean run = true;
+        int option;
+        String inputStr;
+
+        printMenu();
+        option = input.nextInt();
+
+        while (run) {
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter the string: ");
+                    inputStr = input.next();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.exit(1);
+                    break;
+            }
+
+            printMenu();
+            option = input.nextInt();
+        }
+    }
+
+    public static int countVowels(String str) {
+
+        return -1;
+    }
+
+    public static void printMenu() {
+        System.out.println("---------MAIN MENU---------");
+        System.out.println("1. Read input string");
+        System.out.println("2. Compute number of vowels");
+        System.out.println("3. Exit program");
+        System.out.println();
+        System.out.println("Enter option number: ");
+    }
 }
