512f0f0ecac7b5b5acb93342723d4b016b115aae
bwood <bwoods51501@gmail.com>
2022-08-26 11:06:35 -0400

    Change countVowels

--- src/Vowels.java
+++ src/Vowels.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 
 public class Vowels {
 
+    int count = 0;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -16,34 +18,48 @@ public class Vowels {
         int option;
         String inputStr = "";
 
-        printMenu();
-        option = input.nextInt();
+        System.out.println("Enter the string: ");
+        inputStr = input.nextLine();
 
         while (run) {
+
+            printMenu();
+            option = input.nextInt();
 
             switch (option) {
                 case 1 -> {
                     System.out.println("Enter the string: ");
                     inputStr = input.next();
                 }
-                case 2 -> System.out.println("The number of vowels in " + inputStr + " is " +countVowels(inputStr));
+                case 2 -> System.out.println("The number of vowels in " + inputStr + " is " +countVowels(inputStr, inputStr.length()));
                 case 3 -> {
                     run = false;
                     System.exit(1);
                 }
             }
-
-            printMenu();
-            option = input.nextInt();
         }
     }
 
-    public static int countVowels(String str) {
+    static int isVowel(char ch)
+    {
+        ch = Character.toUpperCase(ch);
+        if(ch=='A' || ch=='E' || ch=='I' ||
+                ch=='O' || ch=='U')
+            return 1;
+        else return 0;
+    }
 
-        return -1;
+    // to count total number of vowel from 0 to n
+    static int countVowels(String str, int n)
+    {
+        if (n == 1)
+            return isVowel(str.charAt(n - 1));
+
+        return countVowels(str, n-1) + isVowel(str.charAt(n - 1));
     }
 
     public static void printMenu() {
+        System.out.println();
         System.out.println("---------MAIN MENU---------");
         System.out.println("1. Read input string");
         System.out.println("2. Compute number of vowels");
